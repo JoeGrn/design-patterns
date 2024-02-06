65256f35a7df7de852a1736cb1d50906c042c874
Joseph Green <joseph.peter.green@gmail.com>
2024-02-06 08:43:28 +0000

    feat: init repo, singleton class

---
+++ src/singleton/index.ts
@@ -0,0 +1,26 @@
+export class Singleton {
+    private static instance: Singleton;
+
+    private constructor () {}
+
+    public static initialize(): Singleton {
+        if(!Singleton.instance) {
+            Singleton.instance = new Singleton();
+        }
+
+        return Singleton.instance
+    }
+}
+
+const clientCode = () => {
+    const s1 = Singleton.initialize();
+    const s2 = Singleton.initialize();
+
+    if (s1 === s2) {
+        console.log('Singleton works, both variables contain the same instance.');
+    } else {
+        console.log('Singleton failed, variables contain different instances.');
+    }
+}
+
+clientCode();
